fb837f25158b26ef01e4931a3034942ba6126a27
Satyam Kumar <satyamkr571@gmail.com>
2024-05-01 22:39:47 +0530

    closer explained

--- javaScriptConcept.js
+++ javaScriptConcept.js
@@ -2,3 +2,31 @@
 // freeze(), on the other hand, prevents any modifications, additions, or deletions to properties.Object.
 
 // But we can have all the operation in nested child
+// const comFunction = compose(
+//   () => 1 + 1,
+//   () => 2 + 1
+// );
+// console.log(comFunction);
+
+const timer = (limit) => {
+  for (var i = 0; i < limit; i++) {
+    function x(data) {
+      setTimeout(() => {
+        console.log(data);
+      }, i * 1000);
+    }
+    x(i);
+  }
+};
+
+timer(10);
+
+function x() {
+  let i = 0;
+  setTimeout(() => {
+    console.log(i);
+  }, 3000);
+  console.log("JS");
+}
+
+x();
